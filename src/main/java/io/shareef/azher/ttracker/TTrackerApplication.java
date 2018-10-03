095e6d20224deb2f5f3655b78bdfcd3315c43be9
ashareef <azher.m.shareef@gmail.com>
2018-10-02 21:34:48 -0400

    spring boot init

---
+++ src/main/java/io/shareef/azher/ttracker/TTrackerApplication.java
@@ -0,0 +1,12 @@
+package io.shareef.azher.ttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TTrackerApplication.class, args);
+	}
+}
